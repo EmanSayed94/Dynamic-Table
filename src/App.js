f5192cfebdf0b9001d08c7e378b46dab3ff621d1
EmanSayed94 <emanelsayed.33@yahoo.com>
2020-12-29 15:26:51 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg'
+import './App.css'
+import DynamicTable from './components/Dynamic-Table/index'
+
+function App() {
+  return <DynamicTable />
+}
+
+export default App
---
+++ src/components/Dynamic-Table/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DynamicTable = () => {
+    return ( <></> );
+}
+ 
+export default DynamicTable;
