dd4cb41a60b2985789f57e4d72e189ca22b08858
EmanSayed94 <emanelsayed.33@yahoo.com>
2020-12-29 15:35:31 +0200

    generate dynamic table from input values

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg'
 import './App.css'
-import DynamicTable from './components/Dynamic-Table/index'
+import DynamicTable from './components/DynamicTable/index'
 
 function App() {
   return <DynamicTable />
